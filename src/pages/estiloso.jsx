99edaddc69259c73d362de822e118f3c32fc52de
KDash <miromtd@gmail.com>
2022-09-17 14:45:11 -0300

    UPDATED estiloso.jsx

--- src/pages/estiloso.jsx
+++ src/pages/estiloso.jsx
@@ -1,15 +1,13 @@
 import styles from '../styles/Estiloso.module.css'
-import Link from 'next/link'
+import Layout from '../components/Layout'
 
 
 export default function Estiloso() {
     return (
-        <div className={styles.roxo}>
-            <Link href="/">Voltar</Link>
-            <h1>Componente X </h1>
-            <h6>Estilo usando CSS Módulos</h6>
-            <h3>O componente mais Estiloso do momento!!!</h3>
-
-        </div>
+        <Layout>
+            <div className={styles.roxo}>
+                <h1>Estilos usando CSS Módulos</h1>
+            </div>
+        </Layout>
     )
 }
