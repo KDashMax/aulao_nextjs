67c247bb0250680f38b26aaf8e34ff1f5452ea6e
KDash <miromtd@gmail.com>
2022-09-20 19:05:02 -0300

    UPDATED index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,13 @@
-import Link from "next/link"
+import Navegador from "../components/Navegador"
 
 export default function Inicio() {
-    // "<h1>Título</h1>"
-    // return <h1>Título</h1>
+    
     return (
         <>
             <h1>Fundamentos de Next.js & React</h1>
             <h2>Vamos estudar esse framework</h2>
             <h4>* Páginas</h4>
-            <Link href="/estiloso">
-                Estiloso
-            </Link>
+            <Navegador />
         </>
     )
 }
