65b7f8667eb6c7140bbacf54def8fd2bb6dca203
KDash <miromtd@gmail.com>
2022-09-20 19:50:54 -0300

    UPDATED pages exemplo.jsx

--- src/pages/exemplo.jsx
+++ src/pages/exemplo.jsx
@@ -2,11 +2,11 @@ import Cabecalho from "../components/Cabecalho"
 
 export default function Exemplo() {
     return (
-        <>
+        <Layout titulo="Exemplo Usando Componentes">
             <h1>Exemplo</h1>
             <Cabecalho titulo="Next.js & React"/>
             <Cabecalho titulo="Aprenda Next na prática"/>
-        </>
+        </Layout>
 
     )
 }
