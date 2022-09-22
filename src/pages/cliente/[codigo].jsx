2b1f37bd3cb1e77851c0cf54771c4427d7db6e8c
KDash <miromtd@gmail.com>
2022-09-22 17:18:03 -0300

    UPDATED 0001

--- src/pages/cliente/[codigo].jsx
+++ src/pages/cliente/[codigo].jsx
@@ -1,6 +1,14 @@
 import Layout from "../../components/Layout"
+// import { useRouter } from "next/router"
+import router from "next/router"
+import { useEffect } from "react"
 
 export default function ClienteProCodigo() {
+    // console.log(router.query);
+    useEffect(() => {
+        console.log(router.query);
+        console.log(router.query.codigo);
+    }, [])
     return (
         <Layout titulo="Navegação Dinâmica">
             <span>Código = ????</span>
