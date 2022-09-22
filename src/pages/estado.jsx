6d6f15556422f1d9a54a8b30f33767408476f86c
KDash <miromtd@gmail.com>
2022-09-22 18:06:00 -0300

    ADDED estado.jsx

---
+++ src/pages/estado.jsx
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+
+export default function Estodo() {
+    return (
+        <Layout titulo="Componente com Estado">
+            <span>{numero}</span>
+        </Layout>
+    )
+}
