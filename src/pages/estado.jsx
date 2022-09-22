a5cc8c7d269f6b5b85d79c7f2fd053db6f245828
KDash <miromtd@gmail.com>
2022-09-22 18:14:21 -0300

    UPDATED task 0001

--- src/pages/estado.jsx
+++ src/pages/estado.jsx
@@ -1,9 +1,16 @@
 import Layout from "../components/Layout";
 
 export default function Estodo() {
+    let numero = 3
+
+    function incrementar() {
+        console.log('incrementar')    
+    }
+
     return (
         <Layout titulo="Componente com Estado">
             <span>{numero}</span>
+            <button onClick={incrementar}>Incrementar</button>
         </Layout>
     )
 }
