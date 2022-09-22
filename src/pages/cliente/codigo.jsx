03ea01df57ab65e34d5636d54194e9d402f4c74c
KDash <miromtd@gmail.com>
2022-09-22 16:48:24 -0300

    ADDED cliente/codigo.jsx

---
+++ src/pages/cliente/codigo.jsx
@@ -0,0 +1,9 @@
+import Layout from "../../components/Layout"
+
+export default function ClienteProCodigo() {
+    return (
+        <Layout titulo="Navegação Dinâmica">
+            <span>Código = ????</span>
+        </Layout>
+    )
+}
