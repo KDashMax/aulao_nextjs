8fe3878481f9668f7d72b678d58ceccc252ce84d
KDash <miromtd@gmail.com>
2022-09-26 17:37:17 -0300

    UPDATED testing API 0002, task done

--- src/pages/api/clientes/index.js
+++ src/pages/api/clientes/index.js
@@ -1,7 +1,17 @@
-export default function cliente(req, res) {
+export default function handler(req, res) {
+    
+    if(req.method === 'GET'){
+        handlerGet(req, res)
+    } else {
+        res.status(405).send()
+    }
+
+}
+
+function handlerGet(req, res) {
     res.status(200).json({
         id: 3,
         nome: 'Maria',
-        idade: 33
+        email: 'mariamariamaria@xcfmail.com'
     })
 }
