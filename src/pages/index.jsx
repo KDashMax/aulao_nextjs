0b73512471881742aba12826034e2d9b9f78f416
KDashMax <68829881+KDashMax@users.noreply.github.com>
2022-09-26 19:36:09 -0300

    450 integracao com api (#9)
    * 450 integração com api (#9)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -22,6 +22,7 @@ export default function Inicio() {
                 <Navegador texto="Navegação #01" destino="/navegacao/" cor="green" />
                 <Navegador texto="Navegação #02" destino="/cliente/sp-2/991" cor="navy" />
                 <Navegador texto="Componente com Estado" destino="/estado" cor="hotpink" />
+                <Navegador texto="Integração com API #01" destino="/integracao_1" cor="goldenrod" />
             </div>
         </>
     )
